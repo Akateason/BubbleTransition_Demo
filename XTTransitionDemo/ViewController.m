050dbbfe08df1ff723944deeec87b41cd728371e
teason <akateason@hotmail.com>
2016-04-21 15:31:58 +0800

    Initial Commit

---
+++ XTTransitionDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XTTransitionDemo
+//
+//  Created by teason on 16/4/21.
+//  Copyright © 2016年 teason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XTTransitionDemoTests/XTTransitionDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  XTTransitionDemoTests.m
+//  XTTransitionDemoTests
+//
+//  Created by teason on 16/4/21.
+//  Copyright © 2016年 teason. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XTTransitionDemoTests : XCTestCase
+
+@end
+
+@implementation XTTransitionDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
